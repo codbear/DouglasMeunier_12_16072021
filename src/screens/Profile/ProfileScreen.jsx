55d9e174b3ec2f003fcb889b2f629b46f1ddbf77
codbear <douglas.meunier@openclassrooms.com>
2021-09-19 01:06:22 +0200

    feat(ProfileScreen): create ProfileScreen skeletton

--- src/screens/Profile/ProfileScreen.jsx
+++ src/screens/Profile/ProfileScreen.jsx
@@ -1,7 +1,33 @@
 import { Layout } from '../../components';
+import './ProfileScreen.scss';
+
+const user = {
+  firstName: 'Thomas',
+};
 
 const ProfileScreen = () => {
-  return <Layout />;
+  return (
+    <Layout>
+      <p className="Profile_hello">
+        Bonjour <span className="Profile_hello_firstName">{user.firstName}</span>
+      </p>
+      <p className="Profile_greetings">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+      <div className="Profile_dataGrid">
+        <div className="Profile_dataWrapper Profile_chartsWrapper">
+          <div className="Profile_chart Profile_chart--large">graph 1</div>
+          <div className="Profile_chart">graph 2</div>
+          <div className="Profile_chart">graph 3</div>
+          <div className="Profile_chart">graph 4</div>
+        </div>
+        <div className="Profile_dataWrapper Profile_statsWrapper">
+          <div className="Profile_stat">stat 1</div>
+          <div className="Profile_stat">stat 2</div>
+          <div className="Profile_stat">stat 3</div>
+          <div className="Profile_stat">stat 4</div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default ProfileScreen;
