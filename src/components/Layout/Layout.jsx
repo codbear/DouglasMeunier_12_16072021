7bb74d998bf6efd2d5280b6b72680d175497d59d
codbear <douglas.meunier@openclassrooms.com>
2021-09-18 23:57:13 +0200

    feat(Layout): create Layout component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Header } from '../Header';
+import { SideNav } from '../SideNav';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <SideNav />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
