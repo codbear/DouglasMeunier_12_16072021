ddd6fae7144df62f6f931316a5687d121a033e9e
Codbear <douglas.meunier@openclassrooms.com>
2021-10-08 14:02:30 +0200

    feat(sdk): create useUserAverageSessions hook

--- src/sdk/index.js
+++ src/sdk/index.js
@@ -1,2 +1,3 @@
 export { default as useCurrentUser } from './hooks/useCurrentUser';
 export { default as useUserActivity } from './hooks/useUserActivity';
+export { default as useUserAverageSessions } from './hooks/useUserAverageSessions';
