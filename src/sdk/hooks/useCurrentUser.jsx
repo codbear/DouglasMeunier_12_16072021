468662d587137707ce4dc473071e02d04c74377e
Codbear <douglas.meunier@openclassrooms.com>
2021-10-14 20:40:20 +0200

    refactor(sdk): update useQuery to memoize search params

--- src/sdk/hooks/useCurrentUser.jsx
+++ src/sdk/hooks/useCurrentUser.jsx
@@ -1,8 +1,21 @@
-import useQuery from './useQuery';
+import useSearchParams from './useSearchParams';
 import useGet from './useGet';
 
+/**
+ *
+ * @return {{
+ *  isLoading: boolean,
+ *  currentUser: {
+ *    userInfos: {
+ *      firstName: string,
+ *      lastName: string
+ *    }
+ *  },
+ *  error: Error
+ * }}
+ */
 const useCurrentUser = () => {
-  const query = useQuery();
+  const query = useSearchParams();
   const currentUserId = query.get('userId');
 
   const { isLoading, data: currentUser, error } = useGet('/user/:id', { id: currentUserId });
